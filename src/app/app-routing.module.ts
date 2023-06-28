ee16a663e433c382e37b6e3c7c3c651a6451c80b
Moa Amaro <moacir.amaro@mindlab.com.br>
2023-06-27 22:27:06 -0300

    feat: add new modules to app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthRoutes } from './auth/auth-routing.module';
+import { HomeComponent } from './pages/home/home.component';
+import { MostLikedComponent } from './pages/most-liked/most-liked.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  ...AuthRoutes,
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'most-liked',
+    component: MostLikedComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './pages/home/home.component';
+import { MovieCardComponent } from './shared/movie-card/movie-card.component';
+import { FooterComponent, HeaderComponent } from './shared';
+import { MostLikedComponent } from './pages/most-liked/most-liked.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MovieCardComponent,
+    HeaderComponent,
+    FooterComponent,
+    MostLikedComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
